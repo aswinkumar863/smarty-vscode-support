4aea893948a821d884907cd0b65b3cca7c908dd9
Michael Waack <m.waack@i-d.de>
2020-06-11 22:07:05 +0200

    #8 formatter nested indet

--- client/src/formatter.ts
+++ client/src/formatter.ts
@@ -31,38 +31,29 @@ export class BeautifyHTMLFormatter implements DocumentFormattingEditProvider, Do
 		return [TextEdit.replace(range, formatted)];
 	}
 
-	beautifySmarty(docText: String) {
+	beautifySmarty(docText) {
+		var _a, _b, _c;
 		const startedRegions = [];
-
 		const embeddedRegExp = /<(script|style)[\s\S]*?>[\s\S]*?<\/(script|style)>/g;
 		const smartyRegExp = /^.*{{?.([^{}]|{([^{}])*})*}}?.*$/gm;
-
 		docText = docText.replace(embeddedRegExp, match => {
 			return match.replace(smartyRegExp, "/* beautify ignore:start */$&/* beautify ignore:end */");
 		});
-
 		const regionTag = {
-			foldStartRegex: /{{?(block|capture|for|foreach|function|if|literal|section|setfilter|strip|while)\b.*?}}?/,
-			foldMiddleRegex: /{{?(else(if)?|foreachelse)\b.*?}}?/,
-			foldEndRegex: /{{?\/(block|capture|for|foreach|function|if|literal|section|setfilter|strip|while)\b}}?/,
-			foldBothRegex: /{{?\/?(block|capture|for|foreach|function|if|literal|section|setfilter|strip|while|else|elseif|foreachelse)\b.*?}}?/g
+			foldStartRegex: /{{?(block|capture|for|foreach|function|if|literal|section|setfilter|strip|while)[^}]*}?/,
+			foldMiddleRegex: /{{?(else(if)?|foreachelse)[^}]*}?/,
+			foldEndRegex: /{{?\/(block|capture|for|foreach|function|if|literal|section|setfilter|strip|while)[^}]*}?/,
 		};
-
 		const beautifyOptions = this.getBeautifyOptions();
 		let formatted = beautify(docText, beautifyOptions);
-
 		const lines = formatted.split("\n");
 		const indent_char = beautifyOptions.indent_with_tabs ? "	" : " ".repeat(beautifyOptions.indent_size);
-		let i = 0;
-
-		while (lines[i]) {
+		for (let i = 0; i < lines.length; i++) {
 			let line = lines[i];
 			let reapeat = startedRegions.length;
-
-			let startMatch = line.match(regionTag.foldStartRegex) ?? [];
-			let middleMatch = line.match(regionTag.foldMiddleRegex) ?? [];
-			let endMatch = line.match(regionTag.foldEndRegex) ?? [];
-
+			let startMatch = (_a = line.match(regionTag.foldStartRegex)) !== null && _a !== void 0 ? _a : [];
+			let middleMatch = (_b = line.match(regionTag.foldMiddleRegex)) !== null && _b !== void 0 ? _b : [];
+			let endMatch = (_c = line.match(regionTag.foldEndRegex)) !== null && _c !== void 0 ? _c : [];
 			if (startMatch.length) {
 				startedRegions.push(startMatch[0]);
 			} else if (middleMatch.length) {
@@ -74,18 +65,11 @@ export class BeautifyHTMLFormatter implements DocumentFormattingEditProvider, Do
 			if (startMatch[1] && (startMatch[1] == endMatch[1])) {
 				startedRegions.pop();
 			} else if ((startMatch.length + middleMatch.length + endMatch.length) > 2) {
-				let iter = 0;
-
-				const spaces = lines[i].replace(/^( +).*/s, "$1")
-				const newLines = lines[i].replace(regionTag.foldBothRegex, match => (iter++ ? "\n" + spaces : "") + match).split('\n')
-				lines.splice(i, 1, ...newLines);
+				lines.splice(i, 1, ...lines[i].split('\n'));
 			}
-
 			lines[i] = indent_char.repeat(Math.max(0, reapeat)) + lines[i]
 				.replace(/\/\* beautify ignore:(start|end) \*\/(\s+)?/g, "");
-			i += 1;
 		}
-
 		formatted = lines.join("\n").replace(/\/\* beautify ignore:(start|end) \*\/([ ])?/g, "");
 		return formatted;
 	}
