562471c28d0403418beb36304d3621c56d4b6c38
Jirka Venty <jiri.michel@hotel.cz>
2022-12-11 17:35:55 +0100

    Handle relative paths

--- client/src/language/documentLink.ts
+++ client/src/language/documentLink.ts
@@ -9,21 +9,40 @@ export class DocumentLinkProvider implements DocumentLinkProvider {
 		const linkPattern: RegExp = /(?<=file=['"]).*\.tpl/g;
 
 		for (let match: RegExpExecArray; match = linkPattern.exec(text); match) {
+
+			// Get position in document
+			const range = new Range(
+				document.positionAt(match.index),
+				document.positionAt(match.index + match[0].length)
+			);
+
+			// Get target file path
+			const fPath = match[0];
+
+			// Current file directory path
+			const docdir = path.dirname(document.uri.path);
+
+			// Handle relative paths
+			if (fPath.startsWith('./')) {
+				const absPath = path.posix.resolve(docdir, fPath);
+
+				results.push(
+					new DocumentLink(range, Uri.parse(absPath))
+				);
+
+				continue;
+			}
+
 			const uri = path.posix.normalize(match[0]?.replace(/^[/]?/, '/'));
 
 			const targets = await workspace.findFiles('**' + uri, null, 1);
 
 			// To create file if path not found
 			if (!targets.length) {
-				const docdir = path.dirname(document.uri.path);
 				const pathresolved = path.posix.resolve(docdir, match[0]?.replace(/^[/]?/, ''));
 				targets.push(Uri.parse(pathresolved));
 			}
 
-			const range = new Range(
-				document.positionAt(match.index),
-				document.positionAt(match.index + match[0].length)
-			);
 			const doumentlink = new DocumentLink(range, targets[0]);
 
 			results.push(doumentlink);
