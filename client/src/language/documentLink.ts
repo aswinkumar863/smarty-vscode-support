e8896fd1cb20d3b3025cc81c46e77c2473f0f929
Jirka Venty <jiri.michel@hotel.cz>
2022-12-11 17:39:40 +0100

    Handle shorthand includes

--- client/src/language/documentLink.ts
+++ client/src/language/documentLink.ts
@@ -6,7 +6,7 @@ export class DocumentLinkProvider implements DocumentLinkProvider {
 	async provideDocumentLinks(document: TextDocument, token: CancellationToken) {
 		const results: DocumentLink[] = [];
 		const text: string = document.getText();
-		const linkPattern: RegExp = /(?<=file=['"]).*\.tpl/g;
+		const linkPattern: RegExp = /(?<=['"]).*\.tpl(?=['"])/g;
 
 		for (let match: RegExpExecArray; match = linkPattern.exec(text); match) {
 
